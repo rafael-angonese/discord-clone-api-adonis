14f8260e11c8ad1a4d18dc2a8911bf035e853073
Rafael <rafael_angonese@unochapeco.edu.br>
2021-03-15 22:53:16 -0300

    routes, auth, database, validator

---
+++ app/Controllers/Http/AuthController.ts
@@ -0,0 +1,29 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import User from 'App/Models/User'
+import UserValidator from 'App/Validators/UserValidator'
+
+export default class AuthController {
+  public async register({ request }: HttpContextContract) {
+
+    const data = await request.validate(UserValidator)
+
+    const user = await User.create(data)
+
+    return user.toJSON()
+  }
+
+  public async authenticate({ request, auth }: HttpContextContract) {
+    const email = request.input('email')
+    const password = request.input('password')
+
+    const token = await auth.use('api').attempt(email, password)
+    return token.toJSON()
+  }
+
+  public async logout({ auth }: HttpContextContract) {
+    await auth.use('api').logout()
+
+    // await Database.from('api_tokens').where('id', tokenId).delete()
+  }
+}
--- start/routes.ts
+++ start/routes.ts
@@ -19,7 +19,20 @@
 */
 
 import Route from '@ioc:Adonis/Core/Route'
+import HealthCheck from '@ioc:Adonis/Core/HealthCheck'
 
 Route.get('/', async () => {
   return { hello: 'world' }
 })
+
+Route.get('health', async ({ response }) => {
+  const report = await HealthCheck.getReport()
+
+  return report.healthy ? response.ok(report) : response.badRequest(report)
+})
+
+
+Route.post('register', 'AuthController.register')
+Route.post('authenticate', 'AuthController.authenticate')
+
+Route.get('users', 'UsersController.index').middleware('auth')
