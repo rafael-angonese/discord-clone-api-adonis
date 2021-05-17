02831cfad3827ea463d2e3f5a8a069ad1a787d02
Rafael Angonese <rafael.angonese09@gmail.com>
2021-05-17 00:04:41 -0300

    add ChannelController

---
+++ app/Controllers/Http/ChannelsController.ts
@@ -0,0 +1,61 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+import Channel from 'App/Models/Channel'
+
+export default class ChannelsController {
+  public async index() {
+    const channels = await Channel.all()
+
+    return channels
+  }
+
+  public async store({ request, auth }: HttpContextContract) {
+    const user = auth.user
+
+    const newChannelSchema = schema.create({
+      name: schema.string({ trim: true }, [rules.maxLength(255)]),
+      server_id: schema.number(),
+    })
+
+    const payload = await request.validate({
+      schema: newChannelSchema,
+    })
+
+    const channel = await Channel.create({
+      name: payload.name,
+      serverId: payload.server_id,
+      userId: user!.id,
+    })
+
+    return channel
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const channel = await Channel.findOrFail(params.id)
+
+    return channel
+  }
+
+  public async update({ request, params }: HttpContextContract) {
+    const channel = await Channel.findOrFail(params.id)
+
+    const updateChannelSchema = schema.create({
+      name: schema.string({ trim: true }, [rules.maxLength(255)]),
+    })
+
+    const payload = await request.validate({
+      schema: updateChannelSchema,
+    })
+
+    channel.name = payload.name
+
+    await channel.save()
+
+    return channel
+  }
+
+  public async delete({ params }: HttpContextContract) {
+    const channel = await Channel.findOrFail(params.id)
+    await channel.delete()
+  }
+}
